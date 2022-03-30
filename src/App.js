b416fc580c3a6bf5b40df07621af03a064040771
Quoc Viet <vietpqfx14832@funix.edu.vn>
2022-03-30 15:43:32 +0700

    lab03.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap'
+import Menu from './components/MenuComponent';
+
 
 class App extends Component {
 
@@ -12,6 +14,7 @@ class App extends Component {
               <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
             </div>
           </Navbar>
+          <Menu />
       </div>
     );
   }
