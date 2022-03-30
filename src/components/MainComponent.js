88bfea148c80f6b6102eb6cf1d7f90a7fe737172
Quoc Viet <vietpqfx14832@funix.edu.vn>
2022-03-31 00:35:35 +0700

    lab05.4

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -34,6 +34,13 @@ class Main extends Component {
       />;
     };
 
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
     return (
       <div>
         <Header />
@@ -44,6 +51,7 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact 
           path="/contactus" 
           component={Contact} />
