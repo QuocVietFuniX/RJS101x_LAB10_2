ef3fa7c1c5c7956d025a2ebd2e30b1f3396d926d
Quoc Viet <vietpqfx14832@funix.edu.vn>
2022-03-30 15:51:40 +0700

    LAB03.2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap'
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
 
   render() {
     return (
@@ -14,7 +22,7 @@ class App extends Component {
               <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
             </div>
           </Navbar>
-          <Menu />
+          <Menu dishes={this.state.dishes} />
       </div>
     );
   }
