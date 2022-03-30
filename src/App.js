cd268e40224a170229f20c74a201552e237a92a3
Quoc Viet <vietpqfx14832@funix.edu.vn>
2022-03-30 15:34:47 +0700

    lab02.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap'
+
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-          <h1>Welcome to ReacJS</h1>
+          <Navbar dark color = "primary">
+            <div className='container'>
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
       </div>
     );
   }
