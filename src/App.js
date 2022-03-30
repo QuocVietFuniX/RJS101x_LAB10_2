32aa03b7adbdfd13cef4775a4208c4eb00287031
Quoc Viet <vietpqfx14832@funix.edu.vn>
2022-03-30 15:20:19 +0700

    lab02.1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1>Wellcome to ReacJS</h1>
+          <h1>Welcome to ReacJS</h1>
       </div>
     );
   }
