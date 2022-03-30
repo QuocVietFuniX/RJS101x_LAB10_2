722f31c98a96e02133ae293d03a724078b09cbdf
Quoc Viet <vietpqfx14832@funix.edu.vn>
2022-03-30 22:35:32 +0700

    lab05.2

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,13 +1,66 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, Collapse, NavbarToggler, NavItem, Jumbotron } from 'reactstrap';
+import { NavLink } from 'react-router-dom'
 
 class Header extends Component {
+  constructor(props){
+    super(props);
+
+    this.toggleNav = this.toggleNav.bind(this);
+    this.state = {
+      isNavOpen: false
+    };
+  }
+
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen
+    })
+  }
+
   render() {
     return(
     <React.Fragment>
-      <Navbar dark>
+      <Navbar dark expand="md">
         <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNav} />
+          <NavbarBrand className="mr-auto" href="/">
+            <img
+              src="assets/images/logo.png"
+              height="30"
+              width="41"
+              alt="Ristorante con Fusion"
+            />
+          </NavbarBrand>
+          <Collapse isOpen={this.state.isNavOpen} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink className="nav-link" to="/home">
+                  <span className="fa fa-home fa-lg"></span> Home
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/aboutus">
+                  <span className="fa fa-info fa-lg"></span> About Us
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/menu">
+                  <span className="fa fa-list fa-lg"></span> Menu
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/home">
+                  <span className="fa fa-address-card fa-lg"></span> Contact Us
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/lienhe">
+                  <span className="fa fa-address-card fa-lg"></span> Liên hệ
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
         </div>
       </Navbar>
       <Jumbotron>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,40 +1,40 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
-import DishDetail from "./DishdetailComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 import { DISHES } from "../shared/dishes";
+
+import Home from "./HomeComponent";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 class Main extends Component {
   constructor(props) {
+    console.log("Constructor");
     super(props);
     this.state = {
-      dishes: DISHES,
-      selectedDish: null,
+      dishes: DISHES
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
-  }
-
   render() {
+    const HomePage = (props) => {
+      return <Home />;
+    };
+
     return (
       <div>
         <Header />
-        <Menu
-          dishes={this.state.dishes}
-          onClick={(dishId) => this.onDishSelect(dishId)}
-        />
-        <DishDetail
-          dish={
-            this.state.dishes.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        />
-        <Footer/>
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Redirect to="/home" />
+        </Switch>
+
+        <Footer />
       </div>
     );
   }
